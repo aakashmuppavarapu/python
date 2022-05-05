dd4f07b9a9af84e7fe62374447f3da18653b4aa0
aakashmuppavarapu <muppavarapuaakash48@gmail.com>
2022-05-05 11:51:01 +0530

    lab1

---
+++ lab1.py
@@ -0,0 +1,26 @@
+#Write a program to add and subtract two numbers.
+#Write a program to multiply and divide two numbers.
+#Write a program to find the square and cube of a given number.
+#Write a program to find the square root of a given number.
+#Write a program to find the area and perimeter of a square.
+
+
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+c = int(input("enter side of square:"))
+
+x = a + b
+y = a - b
+p = a ** 2
+q = b ** 3
+r = a ** 0.5
+g = c ** 2
+h = c * 4
+
+print(f"sum of {a} and {b} is {x}")
+print(f"difference of {a} and {b} is {y}")
+print(f"square of {a} is {p}")
+print(f"cube of {b} is {q}")
+print(f"square root of {a} is {r}")
+print(f"area of square:", g)
+print(f"perimeter of square:", h)
